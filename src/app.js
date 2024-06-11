0b14ce8276f5b819700f925ef8056054767310fa
SavajDrashti <savajdrashti2511@gmail.com>
2024-06-11 22:35:16 +0530

    util code

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGINE,
+    credentials: true
+}))
+
+//json ne aapde config karie 6ie exppress sathe
+app.use(express.json({limit: "16kb"}))
+
+//url pase t hi data levana hoi to
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+
+//koii pn file , img k evu hu store karva magu to public assests 6 k je koi pn use kari shake
+app.use(express.static("public"))
+
+app.use(cookieParser())
+
+export { app }
